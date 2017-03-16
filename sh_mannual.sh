2cad260219159813030fe7e2ffcdee49b0cd0266
xu.fangqing <xu.fangqing@foxmail.com>
2017-03-16 16:45:35 +0800

    sh usage: add a para before

--- sh_mannual.sh
+++ sh_mannual.sh
@@ -5,3 +5,7 @@ cat >&2 << __EOF__
 Could not find source code.  Make sure you are running this script from the
 root of the distribution tree.
 __EOF__
+
+# update the shell script input para. e.g. add a new para before old $@
+set -- "add a new para before" "$@"_
+
